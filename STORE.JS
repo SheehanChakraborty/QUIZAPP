c74d65e68767c42cfcd88d574474d717b5b831eb
SheehanChakraborty <77329538+SheehanChakraborty@users.noreply.github.com>
2022-01-21 13:39:52 +0530

    Add files via upload

---
+++ STORE.JS
@@ -0,0 +1,114 @@
+const myQuestions = [
+    {
+        question: "Which team won the first ODI WC？",
+        answers: {
+            a: "West Indies",
+            b: "India",
+            c: "England",
+            d: "Australia"
+        },
+        correctAnswer: "a",
+        correctAnswerResult: "West Indies"
+    },
+    {
+        question: "Which team won the recent T20 World Cup",
+        answers: {
+            a:  "India",
+            b:  "Newzemand" ,
+            c:  "Australia",
+            d:  "South Africa"
+        },
+        correctAnswer: "c",
+        correctAnswerResult: "Australia"
+    },
+    {
+        question: "In which year India Premier League was started",
+        answers: {
+            a: "2007",
+            b: "2010",
+            c: "2009",
+            d: "2008"
+        },
+        correctAnswer: "d",
+        correctAnswerResult: "2008"
+    },
+    {
+        question: " Who was player of the tournament of 1996 Odi cricket World Cup ",
+        answers: {
+            a: "Md Azharuddin(India)",
+            b: "Sanath Jayasuriya(Srilanka)",
+            c: "Wasim Akram(Pakistan)",
+            d: "Ricky Ponying(Austalia)"
+        },
+        correctAnswer: "b",
+        correctAnswerResult: "Sanath Jayasuriya(Srilanka)"
+    },
+    {
+        question: "Who is the recent winner of male ICC Player of the decade(2011-2020)",
+        answers: {
+            a: "Steve Smith(Australia)",
+            b: "Virat Kohli(India)",
+            c: "Kane Williamson (Newzeland) ",
+            d: "Ab devilliers(South Africa)"
+        },
+        correctAnswer: "b",
+        correctAnswerResult: "Virat Kohli(India)"
+    },
+    {
+        question: "Which is the most successful team in Big Bash League",
+        answers: {
+            a: "Peth Scorshers",
+            b: "Sydney Sixers",
+            c: "Stydey Thunders",
+            d: "Both a and b"
+        },
+        correctAnswer: "d",
+        correctAnswerResult: "Both Perth Scorshers and Sydney Sixers have won 3 times therefore option d is correct"
+    },
+    {
+        question: "Which stadium is known as Home of Cricket",
+        answers: {
+            a: "Lords(England)",
+            b: "Gabba(Australia)",
+            c: "Eden Garden(India)",
+            d: "The Oval(England7)"
+        },
+        correctAnswer: "a",
+        correctAnswerResult: "Lords(England)"
+    },
+    {
+        question: "Which is the most successful team in Indian Premier League(IPL)",
+        answers: {
+            a: "Mumbai Indians",
+            b: "Kolkata Knight Riders",
+            c: "Royal Challengers Bangalore",
+            d: "Sunrisers Hyderabad"
+        },
+        correctAnswer: "a",
+        correctAnswerResult: "Mumbai Indians"
+    },
+    {
+        question: " Which players has scored most runs in Caribbean Premier League history",
+        answers: {
+            a: "Chris Gayle ",
+            b: "Dinesh Ramdin ",
+            c: "Lendl Simmons ",
+            d: "Marlon Samuels"
+        },
+        correctAnswer: "c",
+        correctAnswerResult: "Lendl Simons "
+    },
+    {
+        question: "Who has taken most number of wickets in World Test Championship 2019-21 Season",
+        answers: {
+            a: "Trent Boult(Newzeland)",
+            b: "Jasprit Bumrah(India)",
+            c: "Pat Cummins(Australia)",
+            d: "Ravinchandran Ashwin(India)"
+        },
+        correctAnswer: "d",
+        correctAnswerResult: "Ravichandran Ashwin"
+    },
+    
+
+];
